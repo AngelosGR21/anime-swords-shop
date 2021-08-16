0aa4eec428c6055c9d09a6a8469398899044c455
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-08-16 19:54:59 +0300

    adding dynamic functionality for badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,18 +8,23 @@ import {
 } from "@material-ui/core";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 
+import { useContext } from "react";
+import { DataContext } from "../utils/DataProvider";
+
 //IMPORTING STYLES
 import useStyles from "../useStyles";
 
 const Navbar = () => {
+  const { cart } = useContext(DataContext);
   const classes = useStyles();
+
   return (
     <>
-      <AppBar position="static" className={classes.navbar} color="primary">
+      <AppBar position="sticky" className={classes.navbar} color="primary">
         <Toolbar className={classes.toolbar}>
           <Typography variant="h5">Brand</Typography>
           <IconButton>
-            <Badge badgeContent={1} color="error">
+            <Badge badgeContent={cart.length} showZero color="error">
               <ShoppingBasketIcon />
             </Badge>
           </IconButton>
