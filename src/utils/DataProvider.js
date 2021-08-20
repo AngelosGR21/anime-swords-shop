a17ccbb1bdc1326eef2b1de5a1c6906bab8ccdce
AngelosGR21 <77233810+AngelosGR21@users.noreply.github.com>
2021-08-20 15:27:24 +0300

    adding dynamic titles for filter

--- src/utils/DataProvider.js
+++ src/utils/DataProvider.js
@@ -8,9 +8,23 @@ export const DataProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
+
+  //Getting unique values for each sword title
+  const allTitles = ["All", ...new Set(data.map((sword) => sword.animeTitle))];
+
+  const [titles, setTitles] = useState(allTitles);
   return (
     <DataContext.Provider
-      value={{ data, cart, setCart, total, setTotal, cartItems, setCartItems }}
+      value={{
+        data,
+        cart,
+        setCart,
+        total,
+        setTotal,
+        cartItems,
+        setCartItems,
+        titles,
+      }}
     >
       {children}
     </DataContext.Provider>
